080e2f5285eff6e15637aaaed27e97631a19c5f3
KrishXankar-27 <ihavenoenemies.27.thorfi9@gmail.com>
2025-01-05 10:11:23 +0530

    quize

---
+++ chapter_5/practice set/_05.c
@@ -0,0 +1,11 @@
+// quize
+#include <stdio.h>
+int main(){
+
+int a = 4;
+printf("%d\n", a);  
+printf("%d\n", ++a);  
+printf("%d\n", a++);  
+// printf("%d %d %d \n", a, ++a, a++);  
+    return 0;
+}
